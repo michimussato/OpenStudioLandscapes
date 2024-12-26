021fbd7142ac7902dc05b18ccb953acc9f42e348
Michael Mussato <michimussato@gmail.com>
2024-12-26 23:28:49 +0100

    update

---
+++ 10.2/docker-third-party/likec4/setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+git -C /git clone https://github.com/likec4/likec4.git
+git -C /git clone https://github.com/asdf-vm/asdf.git --branch v0.15.0
+
+source /git/asdf/asdf.sh
+
+cd /git/likec4 || exit 1
+asdf plugin add nodejs
+asdf plugin add yarn
+asdf plugin add dprint
+asdf install
+yarn install
+yarn build
+
+exit 0
