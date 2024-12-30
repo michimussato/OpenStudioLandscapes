fba6eaef9d34f8eb0e40f2a59fa0567a380426cb
Michael Mussato <michimussato@gmail.com>
2024-12-30 15:45:50 +0100

    refactor

--- 10.2/base_images/docker-build.sh
+++ 10.2/base_images/docker-build.sh
@@ -3,9 +3,26 @@
 #mkdir -p /data/share/nfs
 #ln -sfn /data/share/nfs /nfs
 
-./repo_base/docker-build.sh
-./repo_base/repo_installer/docker-build.sh
-./repo_base/client_installer/docker-build.sh
-./repo_base/client_installer/generic_runner/docker-build.sh
-./repo_base/likec4_dev/docker-build.sh
-./repo_base/dagster_dev/docker-build.sh
+pushd repo_base || return
+./docker-build.sh
+popd || return
+
+pushd repo_base/repo_installer || return
+./docker-build.sh
+popd || return
+
+pushd repo_base/client_installer || return
+./docker-build.sh
+popd || return
+
+pushd repo_base/client_installer/generic_runner || return
+./docker-build.sh
+popd || return
+
+pushd repo_base/likec4_dev || return
+./docker-build.sh
+popd || return
+
+pushd repo_base/dagster_dev || return
+./docker-build.sh
+popd || return
