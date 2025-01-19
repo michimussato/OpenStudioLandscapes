ca86ac7f7671539152e4ef0cd18fb37bb599a344
Michael Mussato <michimussato@gmail.com>
2025-01-19 13:48:50 +0100

    refactor

--- src/Deadline/deadline_docker/definitions.py
+++ src/Deadline/deadline_docker/definitions.py
@@ -1,8 +1,12 @@
 from dagster import Definitions, load_assets_from_modules
-from Deadline.deadline_docker import assets  # noqa: TID252
+from Deadline.deadline_docker import assets, assets_10_2
 
-all_assets = load_assets_from_modules([assets])
+assets_base = load_assets_from_modules([assets])
+assets_10_2 = load_assets_from_modules([assets_10_2])
 
 defs = Definitions(
-    assets=all_assets,
+    assets=[
+        *assets_base,
+        *assets_10_2,
+    ],
 )
