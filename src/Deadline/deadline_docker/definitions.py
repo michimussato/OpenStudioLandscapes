36378b0f72cc389824319fcd7768c05b77ae5c68
Michael Mussato <michimussato@gmail.com>
2025-01-19 14:36:29 +0100

    refactor

--- src/Deadline/deadline_docker/definitions.py
+++ src/Deadline/deadline_docker/definitions.py
@@ -1,12 +1,30 @@
 from dagster import Definitions, load_assets_from_modules
-from Deadline.deadline_docker import assets, assets_10_2
+from Deadline.deadline_docker import (
+    assets,
+    assets_10_2,
+)
+from Deadline.deadline_docker.third_party import (
+    ayon,
+    dagster,
+    kitsu,
+    likec4,
+)
 
 assets_base = load_assets_from_modules([assets])
 assets_10_2 = load_assets_from_modules([assets_10_2])
+
+assets_ayon = load_assets_from_modules([ayon])
+assets_dagster = load_assets_from_modules([dagster])
+assets_kitsu = load_assets_from_modules([kitsu])
+assets_likec4 = load_assets_from_modules([likec4])
 
 defs = Definitions(
     assets=[
         *assets_base,
         *assets_10_2,
+        *assets_ayon,
+        *assets_dagster,
+        *assets_kitsu,
+        *assets_likec4,
     ],
 )
---
+++ src/Deadline/deadline_docker/third_party/ayon.py
@@ -0,0 +1,117 @@
+import pathlib
+import yaml
+import shutil
+
+from Deadline.deadline_docker.yaml_tags.overrides import *
+from Deadline.deadline_docker.utils import *
+from Deadline.deadline_docker.constants import *
+
+from dagster import (
+    AssetIn,
+    asset,
+    AssetExecutionContext,
+    Output,
+    AssetMaterialization,
+    MetadataValue
+)
+
+
+@asset(
+    group_name="Ayon",
+    compute_kind="python",
+    ins={
+        "env_base": AssetIn(),
+    },
+)
+def compose_ayon_override(
+        context: AssetExecutionContext,
+        env_base: dict,
+) -> dict[str, list[str]]:
+    """
+    """
+
+    parent = pathlib.Path(env_base.get("AYON_DOCKER_COMPOSE"))
+
+    docker_dict = {
+        "services": {
+            "postgres": {
+                "container_name": "ayon-postgres",
+                "hostname": "ayon-postgres",
+                "domainname": env_base.get("ROOT_DOMAIN"),
+                "volumes": [
+                    f"/etc/localtime:/etc/localtime:ro",
+                    f"{env_base.get('NFS_ENTRY_POINT')}/databases/ayon/postgresql/data:/var/lib/postgresql/data",
+                ],
+                "networks": [
+                    "mongodb",
+                    "repository",
+                ],
+            },
+            "redis": {
+                "container_name": "ayon-redis",
+                "hostname": "ayon-redis",
+                "domainname": env_base.get("ROOT_DOMAIN"),
+                "networks": [
+                    "mongodb",
+                    "repository",
+                ],
+            },
+            "server": {
+                "container_name": "ayon-serve",
+                "hostname": "ayon-serve",
+                "domainname": env_base.get("ROOT_DOMAIN"),
+                "ports": OverrideArray([
+                    f"{env_base.get('AYON_PORT_HOST')}:{env_base.get('AYON_PORT_CONTAINER')}",
+                ]),
+                "networks": [
+                    "mongodb",
+                    "repository",
+                ],
+            },
+        },
+    }
+
+    docker_yaml = yaml.dump(docker_dict)
+
+    docker_compose_override = pathlib.Path(
+        DOT_DOCKER_ROOT,
+        "generations",
+        env_base.get("GENERATION", "default"),
+        "docker_compose",
+        context.asset_key.path[-1],
+        "docker-compose.override.yml",
+    )
+
+    docker_compose_override.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(docker_compose_override, "w") as fw:
+        fw.write(docker_yaml)
+
+    cmd_docker_compose_up = [
+        shutil.which("docker"),
+        "compose",
+        "--file",
+        parent.as_posix(),
+        "--project-name",
+        context.asset_key.path[-1],
+        "up",
+        "--remove-orphans",
+    ]
+
+    ret = {
+        "path": [
+            parent.as_posix(),
+            docker_compose_override.as_posix(),
+        ],
+    }
+
+    yield Output(ret)
+
+    yield AssetMaterialization(
+        asset_key=context.asset_key,
+        metadata={
+            context.asset_key.path[-1]: MetadataValue.json(ret),
+            "cmd_docker_compose_up": MetadataValue.path(cmd_list_to_str(cmd_docker_compose_up)),
+            "yaml": MetadataValue.md(f"```yaml\n{docker_yaml}\n```"),
+        },
+    )
