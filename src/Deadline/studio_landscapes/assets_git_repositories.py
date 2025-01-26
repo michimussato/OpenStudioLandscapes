24a96c89294849c9ed97946b249a1557d61143cf
Michael Mussato <michimussato@gmail.com>
2025-01-27 00:06:41 +0100

    refactor

---
+++ src/Deadline/studio_landscapes/assets_git_repositories.py
@@ -0,0 +1,8 @@
+# https://github.com/michimussato/dagster-shared
+# https://github.com/michimussato/docker-graph
+# https://github.com/michimussato/deadline-wrapper
+# https://github.com/michimussato/deadline-dagster (job processor)
+# https://github.com/michimussato/SSLGeneration
+# https://github.com/michimussato/rez-packages
+# https://github.com/michimussato/deadline-fixtures
+# https://github.com/michimussato/deadline-repository-10
