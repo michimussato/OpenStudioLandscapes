a77d8beb643fa3d76469a52b078cc39c63f032a7
Michael Mussato <michimussato@gmail.com>
2025-02-17 08:47:09 +0100

    refactor

---
+++ src/code_locations/LikeC4/LikeC4/jobs.py
@@ -0,0 +1,14 @@
+from dagster import (
+    define_asset_job,
+    AssetSelection,
+)
+
+from OpenStudioLandscapes.open_studio_landscapes.third_party.LikeC4.sensors import asset_to_watch
+
+
+job_LikeC4 = define_asset_job(
+    name="job_LikeC4",
+    # Asset to trigger:
+    # all in this code location minus the foreign one that was loaded into this gRPC
+    selection=AssetSelection.all(include_sources=True) - AssetSelection.assets(asset_to_watch),
+)
