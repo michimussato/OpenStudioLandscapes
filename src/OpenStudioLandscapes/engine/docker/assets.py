c18228de20ca6b9331c5583d5b4714a12eef6aad
Michael Mussato <michimussato@gmail.com>
2025-03-22 17:45:23 +0100

    refactor

--- src/OpenStudioLandscapes/engine/docker/assets.py
+++ src/OpenStudioLandscapes/engine/docker/assets.py
@@ -16,6 +16,7 @@ from dagster import (
 )
 
 from OpenStudioLandscapes.engine.constants import *
+from OpenStudioLandscapes.engine.utils import *
 
 
 """
@@ -117,151 +118,151 @@ def run_builder(
     )
 
 
-@asset(
-    **ASSET_HEADER_BASE,
-    ins={
-        "env": AssetIn(AssetKey([*KEY_BASE, "env"])),
-    },
-    description="https://www.youtube.com/watch?v=fVXkh7NVvww&ab_channel=ProgrammerGuide",
-)
-def run_registry(
-    context: AssetExecutionContext,
-    env: dict,  # pylint: disable=redefined-outer-name
-) -> Generator[Output[Container] | AssetMaterialization, None, None]:
-
-    # Insecure Registries:
-    # https://wiki.archlinux.org/title/Docker
-
-    """
-sudo bash -c 'mkdir -p /etc/docker
-
-cat > /etc/docker/daemon.json << EOF
-{
-  "insecure-registries" : [
-    "192.168.1.162:5000",
-    "127.0.0.1:5000",
-    "localhost:5000",
-    "0.0.0.0:5000",
-    "[::1]:5000",
-    "http://192.168.1.162:5000",
-    "http://127.0.0.1:5000",
-    "http://localhost:5000",
-    "http://0.0.0.0:5000",
-    "http://[::1]:5000"
-  ]
-}
-
-EOF'
-
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-    """
-
-    _container = None
-    container_name = "openstudiolandscapes-registry"
-    # host_port = REGISTRY_PORT_HOST
-
-    containers = docker.container.list()
-
-    context.log.info(containers)
-
-    for container_ in containers:
-        if container_.name == container_name:
-            _container = container_
-
-    if _container is None:
-        container: Container = docker.container.run(
-            detach=True,
-            remove=True,
-            domainname="farm.evil",
-            # network_mode="host",  # does not exist
-            hostname=container_name,
-            image="registry:2",  # 3 is RC
-            name=container_name,
-            publish=[
-                (5000, 5000),
-            ],
-            volumes=(
-                [
-                    (
-                        pathlib.Path(
-                            get_git_root(path=pathlib.Path(__file__)) / "src" / "OpenStudioLandscapes" / "engine" / "docker" / "daemon.json"
-                        ).as_posix(),
-                        "/etc/docker/daemon.json",
-                        "ro",
-                    ),
-                ]
-            ),
-            mounts=(
-                [
-                    (
-                        "source=local-registry-vol",
-                        "destination=/var/lib/registry",
-                    )
-                ]
-            )
-        )
-
-    else:
-        container: Container = _container
-
-    cmd_interactive = [
-        shutil.which("docker"),
-        "exec",
-        "--interactive",
-        "--tty",
-        # container_name,
-        container.id,
-        "sh"
-    ]
-
-    cmd_logs = [
-        shutil.which("docker"),
-        "logs",
-        "--follow",
-        # container_name,
-        container.id,
-    ]
-
-    cmd_stop = [
-        shutil.which("docker"),
-        "container",
-        "stop",
-        # container_name,
-        container.id,
-    ]
-
-    # cmd_rm = [
-    #     shutil.which("docker"),
-    #     # "buildx",
-    #     "rm",
-    #     # container_name,
-    #     container.id,
-    # ]
-
-    # context.pdb.set_trace()
-    # NetworkSettings(bridge='', sandbox_id='267e9c32fe946b9a4665b52627ccf9b26b3f50cedf08920ed0c1e903e1ce77cf', hairpin_mode=False, link_local_ipv6_address='', link_local_ipv6_prefix_length=0, ports={'5000/tcp': [{'HostIp': '0.0.0.0', 'HostPort': '5000'}, {'HostIp': '::', 'HostPort': '5000'}]}, sandbox_key=PosixPath('/var/run/docker/netns/267e9c32fe94'), secondary_ip_addresses=None, secondary_ipv6_addresses=None, endpoint_id='34685ce658074786f8d9d54d9e66ebfd33ce767073c58ae3a6e0a494d0380db2', gateway='172.17.0.1', global_ipv6_address='', global_ipv6_prefix_length=0, ip_address='172.17.0.3', ip_prefix_length=16, ipv6_gateway='', mac_address='9a:47:f7:2f:05:51', networks={'bridge': NetworkInspectResult(ipam_config=None, links=None, aliases=None, network_id='472718e79828dfb762c3b2792802238587d661be25e4e5c7795f150009e15190', endpoint_id='34685ce658074786f8d9d54d9e66ebfd33ce767073c58ae3a6e0a494d0380db2', gateway='172.17.0.1', ip_address='172.17.0.3', ip_prefix_length=16, ipv6_gateway='', global_ipv6_address='', global_ipv6_prefix_length=0, mac_address='9a:47:f7:2f:05:51', driver_options=None)})
-    # ip_address = container.network_settings.ip_address
-    # ports = container.network_settings.ports["5000/tcp"]
-    # for port in ports:
-    #     if port["HostIp"] != "::":
-    #         host_port = port["HostIp"]
-    #         host_ip = port["HostPort"]
-    #         break
-
-    yield Output(container)
-
-    yield AssetMaterialization(
-        asset_key=context.asset_key,
-        metadata={
-            "__".join(context.asset_key.path): MetadataValue.path(str(container)),
-            "dir": MetadataValue.json(dir(container)),
-            "name": MetadataValue.path(container.name),
-            "id": MetadataValue.path(container.id),
-            "api": MetadataValue.path(f"http://localhost:{host_port}/v2"),
-            "interactive": MetadataValue.path(shlex.join(cmd_interactive)),
-            "logs": MetadataValue.path(shlex.join(cmd_logs)),
-            "stop": MetadataValue.path(shlex.join(cmd_stop)),
-            # "rm": MetadataValue.path(shlex.join(cmd_rm)),
-        },
-    )
+# @asset(
+#     **ASSET_HEADER_BASE,
+#     ins={
+#         "env": AssetIn(AssetKey([*KEY_BASE, "env"])),
+#     },
+#     description="https://www.youtube.com/watch?v=fVXkh7NVvww&ab_channel=ProgrammerGuide",
+# )
+# def run_registry(
+#     context: AssetExecutionContext,
+#     env: dict,  # pylint: disable=redefined-outer-name
+# ) -> Generator[Output[Container] | AssetMaterialization, None, None]:
+#
+#     # Insecure Registries:
+#     # https://wiki.archlinux.org/title/Docker
+#
+#     """
+# sudo bash -c 'mkdir -p /etc/docker
+#
+# cat > /etc/docker/daemon.json << EOF
+# {
+#   "insecure-registries" : [
+#     "192.168.1.162:5000",
+#     "127.0.0.1:5000",
+#     "localhost:5000",
+#     "0.0.0.0:5000",
+#     "[::1]:5000",
+#     "http://192.168.1.162:5000",
+#     "http://127.0.0.1:5000",
+#     "http://localhost:5000",
+#     "http://0.0.0.0:5000",
+#     "http://[::1]:5000"
+#   ]
+# }
+#
+# EOF'
+#
+# sudo systemctl daemon-reload
+# sudo systemctl restart docker
+#     """
+#
+#     _container = None
+#     container_name = "openstudiolandscapes-registry"
+#     # host_port = REGISTRY_PORT_HOST
+#
+#     containers = docker.container.list()
+#
+#     context.log.info(containers)
+#
+#     for container_ in containers:
+#         if container_.name == container_name:
+#             _container = container_
+#
+#     if _container is None:
+#         container: Container = docker.container.run(
+#             detach=True,
+#             remove=True,
+#             domainname="farm.evil",
+#             # network_mode="host",  # does not exist
+#             hostname=container_name,
+#             image="registry:2",  # 3 is RC
+#             name=container_name,
+#             publish=[
+#                 (5000, 5000),
+#             ],
+#             volumes=(
+#                 [
+#                     (
+#                         pathlib.Path(
+#                             get_git_root(path=pathlib.Path(__file__)) / "src" / "OpenStudioLandscapes" / "engine" / "docker" / "daemon.json"
+#                         ).as_posix(),
+#                         "/etc/docker/daemon.json",
+#                         "ro",
+#                     ),
+#                 ]
+#             ),
+#             mounts=(
+#                 [
+#                     (
+#                         "source=local-registry-vol",
+#                         "destination=/var/lib/registry",
+#                     )
+#                 ]
+#             )
+#         )
+#
+#     else:
+#         container: Container = _container
+#
+#     cmd_interactive = [
+#         shutil.which("docker"),
+#         "exec",
+#         "--interactive",
+#         "--tty",
+#         # container_name,
+#         container.id,
+#         "sh"
+#     ]
+#
+#     cmd_logs = [
+#         shutil.which("docker"),
+#         "logs",
+#         "--follow",
+#         # container_name,
+#         container.id,
+#     ]
+#
+#     cmd_stop = [
+#         shutil.which("docker"),
+#         "container",
+#         "stop",
+#         # container_name,
+#         container.id,
+#     ]
+#
+#     # cmd_rm = [
+#     #     shutil.which("docker"),
+#     #     # "buildx",
+#     #     "rm",
+#     #     # container_name,
+#     #     container.id,
+#     # ]
+#
+#     # context.pdb.set_trace()
+#     # NetworkSettings(bridge='', sandbox_id='267e9c32fe946b9a4665b52627ccf9b26b3f50cedf08920ed0c1e903e1ce77cf', hairpin_mode=False, link_local_ipv6_address='', link_local_ipv6_prefix_length=0, ports={'5000/tcp': [{'HostIp': '0.0.0.0', 'HostPort': '5000'}, {'HostIp': '::', 'HostPort': '5000'}]}, sandbox_key=PosixPath('/var/run/docker/netns/267e9c32fe94'), secondary_ip_addresses=None, secondary_ipv6_addresses=None, endpoint_id='34685ce658074786f8d9d54d9e66ebfd33ce767073c58ae3a6e0a494d0380db2', gateway='172.17.0.1', global_ipv6_address='', global_ipv6_prefix_length=0, ip_address='172.17.0.3', ip_prefix_length=16, ipv6_gateway='', mac_address='9a:47:f7:2f:05:51', networks={'bridge': NetworkInspectResult(ipam_config=None, links=None, aliases=None, network_id='472718e79828dfb762c3b2792802238587d661be25e4e5c7795f150009e15190', endpoint_id='34685ce658074786f8d9d54d9e66ebfd33ce767073c58ae3a6e0a494d0380db2', gateway='172.17.0.1', ip_address='172.17.0.3', ip_prefix_length=16, ipv6_gateway='', global_ipv6_address='', global_ipv6_prefix_length=0, mac_address='9a:47:f7:2f:05:51', driver_options=None)})
+#     # ip_address = container.network_settings.ip_address
+#     # ports = container.network_settings.ports["5000/tcp"]
+#     # for port in ports:
+#     #     if port["HostIp"] != "::":
+#     #         host_port = port["HostIp"]
+#     #         host_ip = port["HostPort"]
+#     #         break
+#
+#     yield Output(container)
+#
+#     yield AssetMaterialization(
+#         asset_key=context.asset_key,
+#         metadata={
+#             "__".join(context.asset_key.path): MetadataValue.path(str(container)),
+#             "dir": MetadataValue.json(dir(container)),
+#             "name": MetadataValue.path(container.name),
+#             "id": MetadataValue.path(container.id),
+#             # "api": MetadataValue.path(f"http://localhost:{host_port}/v2"),
+#             "interactive": MetadataValue.path(shlex.join(cmd_interactive)),
+#             "logs": MetadataValue.path(shlex.join(cmd_logs)),
+#             "stop": MetadataValue.path(shlex.join(cmd_stop)),
+#             # "rm": MetadataValue.path(shlex.join(cmd_rm)),
+#         },
+#     )
