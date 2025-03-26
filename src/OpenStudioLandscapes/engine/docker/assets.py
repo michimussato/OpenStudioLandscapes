17bc1f02060bc38d9e976d50e7e3424d97b2bde8
Michael Mussato <michimussato@gmail.com>
2025-03-26 11:08:34 +0100

    refactor

--- src/OpenStudioLandscapes/engine/docker/assets.py
+++ src/OpenStudioLandscapes/engine/docker/assets.py
@@ -1,6 +1,7 @@
 import pathlib
 import shlex
 import shutil
+import textwrap
 from typing import Generator
 
 from python_on_whales import docker, Builder
@@ -122,151 +123,240 @@ def run_builder(
 
 
 if DOCKER_CONFIG.value["docker_use_local"]:
+    # @asset(
+    #     **ASSET_HEADER_BASE,
+    #     # name="Run_Local_Registry",
+    #     ins={
+    #         "env": AssetIn(AssetKey([*KEY_BASE, "env"])),
+    #         # "docker_config": AssetIn(AssetKey([*KEY_BASE, "docker_config"])),
+    #     },
+    #     description="https://www.youtube.com/watch?v=fVXkh7NVvww&ab_channel=ProgrammerGuide",
+    # )
+    # def run_registry(
+    #     context: AssetExecutionContext,
+    #     env: dict,  # pylint: disable=redefined-outer-name
+    #     # docker_config: DockerConfig,  # pylint: disable=redefined-outer-name
+    # ) -> Generator[Output[dict] | AssetMaterialization, None, None]:
+    #     # Todo
+    #     #  - [ ] Write daemon for this
+    #
+    #     docker_config = DOCKER_CONFIG
+    #
+    #     # Target command:
+    #     # /usr/bin/docker container run \
+    #     #     --domainname farm.evil \
+    #     #     --hostname openstudiolandscapes-registry \
+    #     #     --name openstudiolandscapes-registry \
+    #     #     --rm \
+    #     #     --publish 5000:5000 \
+    #     #     --volume /home/michael/git/repos/OpenStudioLandscapes/src/OpenStudioLandscapes/engine/docker/daemon.json:/etc/docker/daemon.json:ro \
+    #     #     --mount source=local-registry-vol,destination=/var/lib/registry \
+    #     #     registry:2
+    #
+    #     # Insecure Registries:
+    #     # https://wiki.archlinux.org/title/Docker
+    #
+    #     client = docker_py.from_env()
+    #     containers = client.containers.list()
+    #
+    #     domainname = "farm.evil"
+    #     host_name="openstudiolandscapes-registry"
+    #     container_name="openstudiolandscapes-registry"
+    #
+    #     repo_dir = pathlib.Path(
+    #         env["DOT_LANDSCAPES"],
+    #         env.get("LANDSCAPE", "default"),
+    #         f"{GROUP_BASE}__{'__'.join(KEY_BASE)}",
+    #         "__".join(context.asset_key.path),
+    #         "repo_dir",
+    #     )
+    #
+    #     repo_dir.parent.mkdir(parents=True, exist_ok=True)
+    #
+    #     # stopping running instance helpful for debugging probably.
+    #     # if not helpful, just return container instance
+    #     for container in containers:
+    #         if container.name == container_name:
+    #             context.log.info(f"Container {container.name} is running already.")
+    #             context.log.info(f"Stopping...")
+    #             container.stop()
+    #             context.log.info(f"Stopped.")
+    #             try:
+    #                 container.remove(
+    #                     force=True,
+    #                 )
+    #             except Exception as e:
+    #                 context.log.warning(e)
+    #             finally:
+    #                 context.log.info(f"Removed.")
+    #
+    #     volumes=[
+    #         (
+    #             pathlib.Path(
+    #                 get_git_root(path=pathlib.Path(
+    #                     __file__)) / "src" / "OpenStudioLandscapes" / "engine" / "docker" / "daemon.json"
+    #             ).as_posix(),
+    #             "/etc/docker/daemon.json",
+    #             "ro",
+    #         ),
+    #         (
+    #             repo_dir.as_posix(),
+    #             "/var/lib/registry",
+    #             "rw",
+    #         ),
+    #     ]
+    #     mounts=[
+    #         # {
+    #         #     "source": "local-registry-vol",
+    #         #     "target": "/var/lib/registry",
+    #         # }
+    #     ]
+    #
+    #     publish = {
+    #         f"{str(docker_config.value['docker_registry_port']) or '5000'}": 5000,
+    #     }
+    #
+    #     volumes_ = []
+    #     for volume in volumes:
+    #         volumes_.append(
+    #             ":".join(volume),
+    #         )
+    #
+    #     mounts_ = []
+    #     for mount in mounts:
+    #         mounts_.append(
+    #             Mount(**mount)
+    #         )
+    #
+    #     container_registry = client.containers.run(
+    #         image="docker.io/registry:2",
+    #         remove=True,
+    #         detach=True,
+    #         domainname=domainname,
+    #         hostname=host_name,
+    #         name=container_name,
+    #         ports=publish,
+    #         stream=True,
+    #         volumes=volumes_,
+    #         mounts=mounts_,
+    #     )
+    #
+    #     context.log.info(dir(container_registry))
+    #
+    #     cmd_interactive = [
+    #         shutil.which("docker"),
+    #         "exec",
+    #         "--interactive",
+    #         "--tty",
+    #         container_registry.id,
+    #         "sh"
+    #     ]
+    #
+    #     cmd_logs = [
+    #         shutil.which("docker"),
+    #         "logs",
+    #         "--follow",
+    #         container_registry.id,
+    #     ]
+    #
+    #     yield Output(container_registry.attrs)
+    #
+    #     yield AssetMaterialization(
+    #         asset_key=context.asset_key,
+    #         metadata={
+    #             "__".join(context.asset_key.path): MetadataValue.json(container_registry.attrs),
+    #             "container_id": MetadataValue.path(container_registry.id),
+    #             "container_name": MetadataValue.path(container_registry.name),
+    #             "cmd_interactive": MetadataValue.path(shlex.join(cmd_interactive)),
+    #             "cmd_logs": MetadataValue.path(shlex.join(cmd_logs)),
+    #         },
+    #     )
     @asset(
         **ASSET_HEADER_BASE,
-        # name="Run_Local_Registry",
         ins={
             "env": AssetIn(AssetKey([*KEY_BASE, "env"])),
-            # "docker_config": AssetIn(AssetKey([*KEY_BASE, "docker_config"])),
         },
         description="https://www.youtube.com/watch?v=fVXkh7NVvww&ab_channel=ProgrammerGuide",
     )
-    def run_registry(
+    def write_registry_env(
         context: AssetExecutionContext,
         env: dict,  # pylint: disable=redefined-outer-name
-        # docker_config: DockerConfig,  # pylint: disable=redefined-outer-name
-    ) -> Generator[Output[dict] | AssetMaterialization, None, None]:
-        # Todo
-        #  - [ ] Write daemon for this
+    ) -> Generator[Output[pathlib.Path] | AssetMaterialization, None, None]:
 
         docker_config = DOCKER_CONFIG
 
-        # Target command:
-        # /usr/bin/docker container run \
-        #     --domainname farm.evil \
-        #     --hostname openstudiolandscapes-registry \
-        #     --name openstudiolandscapes-registry \
-        #     --rm \
-        #     --publish 5000:5000 \
-        #     --volume /home/michael/git/repos/OpenStudioLandscapes/src/OpenStudioLandscapes/engine/docker/daemon.json:/etc/docker/daemon.json:ro \
-        #     --mount source=local-registry-vol,destination=/var/lib/registry \
-        #     registry:2
-
-        # Insecure Registries:
-        # https://wiki.archlinux.org/title/Docker
-
-        client = docker_py.from_env()
-        containers = client.containers.list()
-
-        domainname = "farm.evil"
-        host_name="openstudiolandscapes-registry"
-        container_name="openstudiolandscapes-registry"
-
-        repo_dir = pathlib.Path(
-            env["DOT_LANDSCAPES"],
-            env.get("LANDSCAPE", "default"),
-            f"{GROUP_BASE}__{'__'.join(KEY_BASE)}",
-            "__".join(context.asset_key.path),
-            "repo_dir",
+        daemon_json: tuple = (
+            pathlib.Path(
+                get_git_root(path=pathlib.Path(
+                    __file__)) / "src" / "OpenStudioLandscapes" / "engine" / "docker" / "daemon.json"
+            ).as_posix(),
+            "/etc/docker/daemon.json",
+            "ro",
         )
 
-        repo_dir.parent.mkdir(parents=True, exist_ok=True)
-
-        # stopping running instance helpful for debugging probably.
-        # if not helpful, just return container instance
-        for container in containers:
-            if container.name == container_name:
-                context.log.info(f"Container {container.name} is running already.")
-                context.log.info(f"Stopping...")
-                container.stop()
-                context.log.info(f"Stopped.")
-                try:
-                    container.remove(
-                        force=True,
-                    )
-                except Exception as e:
-                    context.log.warning(e)
-                finally:
-                    context.log.info(f"Removed.")
-
-        volumes=[
-            (
-                pathlib.Path(
-                    get_git_root(path=pathlib.Path(
-                        __file__)) / "src" / "OpenStudioLandscapes" / "engine" / "docker" / "daemon.json"
-                ).as_posix(),
-                "/etc/docker/daemon.json",
-                "ro",
-            ),
-            (
-                repo_dir.as_posix(),
-                "/var/lib/registry",
-                "rw",
-            ),
-        ]
-        mounts=[
-            # {
-            #     "source": "local-registry-vol",
-            #     "target": "/var/lib/registry",
-            # }
-        ]
-
-        publish = {
-            f"{str(docker_config.value['docker_registry_port']) or '5000'}": 5000,
-        }
-
-        volumes_ = []
-        for volume in volumes:
-            volumes_.append(
-                ":".join(volume),
-            )
-
-        mounts_ = []
-        for mount in mounts:
-            mounts_.append(
-                Mount(**mount)
-            )
-
-        container_registry = client.containers.run(
-            image="docker.io/registry:2",
-            remove=True,
-            detach=True,
-            domainname=domainname,
-            hostname=host_name,
-            name=container_name,
-            ports=publish,
-            stream=True,
-            volumes=volumes_,
-            mounts=mounts_,
+        registry_root = pathlib.Path(
+            get_git_root(__file__),
+            ".registry",
+            "registry_root",
         )
 
-        context.log.info(dir(container_registry))
+        registry_root.mkdir(parents=True, exist_ok=True)
 
-        cmd_interactive = [
-            shutil.which("docker"),
-            "exec",
-            "--interactive",
-            "--tty",
-            container_registry.id,
-            "sh"
-        ]
+        registry_env = pathlib.Path(
+            get_git_root(__file__),
+            ".registry",
+            "registry.env",
+        )
 
-        cmd_logs = [
-            shutil.which("docker"),
-            "logs",
-            "--follow",
-            container_registry.id,
-        ]
+        registry_env.parent.mkdir(parents=True, exist_ok=True)
 
-        yield Output(container_registry.attrs)
+        registry_root_volume: tuple = (
+            registry_root.as_posix(),
+            "/var/lib/registry",
+            "rw",
+        )
+
+        registry_env_str = textwrap.dedent(
+            """
+            DOMAIN_NAME="{domain_name}"
+            HOST_NAME="{host_name}"
+            CONTAINER_NAME="{container_name}"
+            PUBLISH="{publish}"
+            DAEMON_JSON="{daemon_json}"
+            REGISTRY_ROOT="{registry_root}"
+            REGISTRY="{registry}"
+            """
+        ).format(
+            domain_name = "farm.evil",
+            host_name="openstudiolandscapes-registry",
+            container_name="openstudiolandscapes-registry",
+            # publish=f"{str(docker_config.value['docker_registry_port']) or '5000'}:5000",
+            publish="5010:5000",
+            daemon_json=":".join(daemon_json),
+            registry_root=":".join(registry_root_volume),
+            registry="docker.io/registry:2",
+        )
+
+        with open(registry_env, "w") as fw:
+            fw.write(registry_env_str)
+
+        with open(registry_env, "r") as fr:
+            registry_env_content = fr.read()
+
+        cmd_restart = f"sudo {shutil.which('systemctl')} restart openstudiolandscapes-registry.service"
+        cmd_journalctl = f"sudo {shutil.which('journalctl')} --follow --unit openstudiolandscapes-registry.service"
+
+        yield Output(registry_env)
 
         yield AssetMaterialization(
             asset_key=context.asset_key,
             metadata={
-                "__".join(context.asset_key.path): MetadataValue.json(container_registry.attrs),
-                "container_id": MetadataValue.path(container_registry.id),
-                "container_name": MetadataValue.path(container_registry.name),
-                "cmd_interactive": MetadataValue.path(shlex.join(cmd_interactive)),
-                "cmd_logs": MetadataValue.path(shlex.join(cmd_logs)),
+                "__".join(context.asset_key.path): MetadataValue.path(registry_env),
+                "registry_env": MetadataValue.md(
+                    f"```shell\n{registry_env_content}\n```"
+                ),
+                "registry_root": MetadataValue.path(registry_root),
+                "cmd_restart": MetadataValue.path(cmd_restart),
+                "cmd_journalctl": MetadataValue.path(cmd_journalctl),
             },
         )
---
+++ src/OpenStudioLandscapes/engine/docker/install_service.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+
+# TOC
+# max-concurrent-uploads:
+# # Limit concurrent uploads:
+# # - https://stackoverflow.com/a/48348339
+# features:
+# # https://docs.docker.com/build/buildkit/#getting-started
+
+
+sudo bash -c 'cat > /usr/lib/systemd/system/openstudiolandscapes-registry.service << EOF
+# https://www.baeldung.com/linux/systemd-services-environment-variables
+[Unit]
+Description=OpenStudioLandscapes Docker Registry Service
+Documentation=
+Requires=docker.service
+
+[Service]
+Type=simple
+EnvironmentFile=/home/michael/git/repos/OpenStudioLandscapes/.registry/registry.env
+WorkingDirectory=/home/michael/git/repos/OpenStudioLandscapes/.registry
+ExecStart=/usr/bin/docker container run --domainname \${DOMAIN_NAME} --hostname \${HOST_NAME} --name \${CONTAINER_NAME} --rm --publish \${PUBLISH} --volume \${DAEMON_JSON} --volume \${REGISTRY_ROOT} \${REGISTRY}
+ExecReload=/bin/kill -HUP \${MAINPID}
+
+[Install]
+WantedBy=multi-user.target
+
+EOF'
+
+sudo systemctl daemon-reload
+sudo systemctl enable openstudiolandscapes-registry.service
+sudo systemctl restart openstudiolandscapes-registry.service
+sudo journalctl -fu openstudiolandscapes-registry.service
