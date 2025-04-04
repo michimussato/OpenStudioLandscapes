b58281549b63de41392f7efb503014e219e518ed
Michael Mussato <michimussato@gmail.com>
2025-04-04 17:43:34 +0200

    refactor

--- generate_readme.sh
+++ generate_readme.sh
@@ -6,15 +6,19 @@
 # OpenStudioLandscapes/src/OpenStudioLandscapes/engine/utils/markdown.py
 
 
-export OSL_ROOT=~/git/repos
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# The script assumes that all module repos live in the same root dir
+# as this one
 
 
-for dir in "${OSL_ROOT}"/OpenStudioLandscapes-*/; do
+for dir in "${SCRIPT_DIR}"/../OpenStudioLandscapes-*/; do
     pushd "${dir}" || exit
     source .venv/bin/activate
     echo "activated."
     if [[ -f generate_readme.py ]]; then
+        echo "Updating ${dir}README.md..."
         python3.11 generate_readme.py
+        echo "Update done."
     else
         echo "ERROR: generate_readme.py does not exist in ${dir}"
     fi;
