da9bddcc9a729b83cd455758f4a1c569880dfc7b
Michael Mussato <michimussato@gmail.com>
2025-04-26 10:58:45 +0200

    refactor

--- install/install_ubuntu_2004-2.sh
+++ install/install_ubuntu_2004-2.sh
@@ -11,17 +11,17 @@ export PYTHON_PAT="11"
 
 apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
 
-pushd $(mktemp -d)
+pushd "$(mktemp -d)" || exit
 
 curl "https://www.python.org/ftp/python/${PYTHON_MAJ}.${PYTHON_MIN}.${PYTHON_PAT}/Python-${PYTHON_MAJ}.${PYTHON_MIN}.${PYTHON_PAT}.tgz" -o Python-${PYTHON_MAJ}.${PYTHON_MIN}.${PYTHON_PAT}.tgz
 tar -xvf Python-${PYTHON_MAJ}.${PYTHON_MIN}.${PYTHON_PAT}.tgz
-cd Python-${PYTHON_MAJ}.${PYTHON_MIN}.${PYTHON_PAT}
+cd Python-${PYTHON_MAJ}.${PYTHON_MIN}.${PYTHON_PAT} || exit
 
 ./configure --enable-optimizations
-make -j $(nproc)
+make -j "$(nproc)"
 make altinstall
 
-popd
+popd || exit
 
 # Install Docker
 # https://docs.docker.com/engine/install/ubuntu/
@@ -41,16 +41,6 @@ apt-get update
 
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Required while not public
-# # 1. https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
-# ssh-keygen -t ed25519 -C "michimussato@gmail.com"
-# eval "$(ssh-agent -s)"
-# ssh-add ~/.ssh/id_ed25519
-# # 2. https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account
-# # 3. https://github.com/settings/keys
-# cat ~/.ssh/id_ed25519.pub
-# ssh-keyscan github.com >> ~/.ssh/known_hosts
-
 
 python3.11 -m venv .venv
 source .venv/bin/activate
@@ -62,5 +52,3 @@ nox -s install_features_into_engine
 
 nox --sessions pi_hole_prepare
 nox --session harbor_prepare
-
-# nox --sessions harbor_up_detach pi_hole_up_detach dagster_postgres_up_detach dagster_postgres
