a6376386d76e7149ab5e075e4b63dc3ff5f2ecac
Michael Mussato <michimussato@gmail.com>
2025-07-30 11:17:23 +0200

    - issue 12: harden .overrides implementation

--- src/OpenStudioLandscapes/engine/exceptions/__init__.py
+++ src/OpenStudioLandscapes/engine/exceptions/__init__.py
@@ -1,4 +1,11 @@
-class ComposeScopeException(Exception):
+class OpenStudioLandscapesException(Exception):
+    """
+    This is the base class for all OpenStudioLandscapes exceptions.
+    """
+    pass
+
+
+class ComposeScopeException(OpenStudioLandscapesException):
     """
     This Exception is raised when a compose_scope is unavailable.
     That means that a module might not be enabled in
