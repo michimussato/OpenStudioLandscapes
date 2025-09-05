2a5b89a382232583dd1c8f4e425d30c5ff20301b
Michael Mussato <michimussato@gmail.com>
2025-09-05 21:34:37 +0200

    - issue 18: add _version.py and github-cli to install_ubuntu_2204.py

--- installer/ubuntu/22.04/install_ubuntu_2204.py
+++ installer/ubuntu/22.04/install_ubuntu_2204.py
@@ -191,6 +191,24 @@ def script_prep() -> pathlib.Path:
             ]
         )
 
+        # GitHub CLI
+        # https://github.com/cli/cli/blob/trunk/docs/install_linux.md#recommended-official
+        script.writelines(
+            [
+                "\n",
+                "\n",
+                "(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \\\n",
+	            "    && sudo mkdir -p -m 755 /etc/apt/keyrings \\\n",
+	            "    && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \\\n",
+	            "    && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \\\n",
+	            "    && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \\\n",
+	            "    && sudo mkdir -p -m 755 /etc/apt/sources.list.d \\\n",
+	            "    && echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main\" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \\\n",
+	            "    && sudo apt update \\\n",
+	            "    && sudo apt install gh -y\n",
+            ]
+        )
+
         script.writelines(
             [
                 "\n",
