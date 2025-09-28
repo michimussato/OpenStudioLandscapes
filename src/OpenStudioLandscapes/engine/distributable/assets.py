9b2a9a7356f3835ef23991c4f76b2538d08a899f
Michael Mussato <michimussato@gmail.com>
2025-09-28 12:15:28 +0200

    - feature-openstudiolandscapes-teleport: improve distributable creation

--- src/OpenStudioLandscapes/engine/distributable/assets.py
+++ src/OpenStudioLandscapes/engine/distributable/assets.py
@@ -17,6 +17,24 @@ from OpenStudioLandscapes.engine.constants import *
 from OpenStudioLandscapes.engine.enums import *
 
 
+def add_file(
+        context,
+        distributable_zip,
+        file_name,
+        arcname,
+) -> None:
+    try:
+        # Add file to zip
+        distributable_zip.write(
+            filename=file_name,
+            arcname=arcname,
+        )
+    except PermissionError as e:
+        context.log.error(e)
+    except OSError as e:
+        context.log.error(e)
+
+
 @asset(
     **ASSET_HEADER_DISTRIBUTABLE,
     ins={
@@ -63,10 +81,92 @@ def distributable(
             context.log.info(f"Creating Distributable...")
             context.log.info(f"{base_landscapes.as_posix() = }")
             context.log.info(f"{landscape_id = }")
-            # Todo:
-            #  - [ ] contolling what ends up in the Zip file
-            #        and what does not could be more dynamic
-            for file_path in base_landscapes.rglob("*"):
+
+            # Todo
+            #  - [ ] This whole logic seems a bit clunky. Could be done better for sure
+
+            # Process all the .files and .directories
+            for file_path in base_landscapes.glob(".*"):
+
+                for file_path_glob in file_path.rglob("*"):
+
+                    context.log.error(f"{file_path_glob = }")
+
+                    # Skip if...
+                    if file_path_glob.name == ".gitkeep":
+                        context.log.info(f"File skipped: {file_path_glob.as_posix()}")
+                        continue
+
+                    if file_path_glob.name == ".gitignore":
+                        context.log.info(f"File skipped: {file_path_glob.as_posix()}")
+                        continue
+
+                    if (
+                        base_landscapes.joinpath(".acme.sh").as_posix()
+                        in file_path_glob.as_posix()
+                    ):
+                        if not base_landscapes.joinpath(".acme.sh") == file_path_glob:
+                            if not file_path_glob.match("*_ecc/*"):
+                                context.log.warning(f"File skipped: {file_path_glob.as_posix()}")
+                                continue
+
+                    if (
+                        base_landscapes.joinpath(".dagster", "postgres").as_posix()
+                        in file_path_glob.as_posix()
+                    ):
+                        if (
+                            not base_landscapes.joinpath(".dagster", "postgres")
+                            == file_path_glob
+                        ):
+                            context.log.warning(f"File skipped: {file_path_glob.as_posix()}")
+                            continue
+
+                    if base_landscapes.joinpath(".n8n").as_posix() in file_path_glob.as_posix():
+                        if not base_landscapes.joinpath(".n8n") == file_path_glob:
+                            context.log.warning(f"File skipped: {file_path_glob.as_posix()}")
+                            continue
+
+                    if (
+                        base_landscapes.joinpath(shared_volumes).as_posix()
+                        in file_path_glob.as_posix()
+                    ):
+                        if (
+                            len(
+                                file_path_glob.relative_to(
+                                    base_landscapes.joinpath(shared_volumes)
+                                ).parts
+                            )
+                            > 1
+                        ):
+                            # Just add the base dir of the shared volumes but not its contents
+                            context.log.warning(f"File skipped: {file_path_glob.as_posix()}")
+                            continue
+
+                    if base_landscapes.joinpath(".n8n").as_posix() in file_path_glob.as_posix():
+                        if not base_landscapes == file_path_glob.parent:
+                            context.log.warning(f"File skipped: {file_path_glob.as_posix()}")
+                            continue
+
+                    context.log.warning(f"Adding file: {file_path_glob.as_posix()}")
+
+                    add_file(
+                        context=context,
+                        distributable_zip=distributable_zip,
+                        file_name=file_path_glob,
+                        arcname=file_path_glob.relative_to(base_landscapes),
+                    )
+
+            # Proecess the Landscape and ignore all other
+            # Landscapes that live inside the `base_landscape` directory
+            for file_path in base_landscapes.joinpath(landscape_id).rglob("*"):
+
+                context.log.error(f"{file_path = }")
+
+                context.log.debug(f"{base_landscapes = }")
+                context.log.debug(f"{file_path = }")
+
+                parts = file_path.relative_to(base_landscapes).parts
+                context.log.debug(f"{parts = }")
 
                 # skip the actual Zip file to prevent
                 # recursion
@@ -74,73 +174,14 @@ def distributable(
                     context.log.info(f"File skipped: {file_path.as_posix()}")
                     continue
 
-                # Skip if...
-                if file_path.name == ".gitkeep":
-                    context.log.info(f"File skipped: {file_path.as_posix()}")
-                    continue
-
-                if file_path.name == ".gitignore":
-                    context.log.info(f"File skipped: {file_path.as_posix()}")
-                    continue
-
-                if (
-                    base_landscapes.joinpath(".acme.sh").as_posix()
-                    in file_path.as_posix()
-                ):
-                    if not base_landscapes.joinpath(".acme.sh") == file_path:
-                        if not file_path.match("*_ecc/*"):
-                            context.log.warning(f"File skipped: {file_path.as_posix()}")
-                            continue
-
-                if (
-                    base_landscapes.joinpath(".dagster", "postgres").as_posix()
-                    in file_path.as_posix()
-                ):
-                    if (
-                        not base_landscapes.joinpath(".dagster", "postgres")
-                        == file_path
-                    ):
-                        context.log.warning(f"File skipped: {file_path.as_posix()}")
-                        continue
-
-                if base_landscapes.joinpath(".n8n").as_posix() in file_path.as_posix():
-                    if not base_landscapes.joinpath(".n8n") == file_path:
-                        context.log.warning(f"File skipped: {file_path.as_posix()}")
-                        continue
-
-                if (
-                    base_landscapes.joinpath(shared_volumes).as_posix()
-                    in file_path.as_posix()
-                ):
-                    if (
-                        len(
-                            file_path.relative_to(
-                                base_landscapes.joinpath(shared_volumes)
-                            ).parts
-                        )
-                        > 1
-                    ):
-                        # Just add the base dir of the shared volumes but not its contents
-                        context.log.warning(f"File skipped: {file_path.as_posix()}")
-                        continue
-
-                if base_landscapes.joinpath(".n8n").as_posix() in file_path.as_posix():
-                    if not base_landscapes == file_path.parent:
-                        context.log.warning(f"File skipped: {file_path.as_posix()}")
-                        continue
-
                 context.log.info(f"Adding {file_path.as_posix()}")
 
-                try:
-                    # Add file to zip
-                    distributable_zip.write(
-                        filename=file_path,
-                        arcname=file_path.relative_to(base_landscapes),
-                    )
-                except PermissionError as e:
-                    context.log.error(e)
-                except OSError as e:
-                    context.log.error(e)
+                add_file(
+                    context=context,
+                    distributable_zip=distributable_zip,
+                    file_name=file_path,
+                    arcname=file_path.relative_to(base_landscapes),
+                )
 
     yield Output(distributable_out)
 
