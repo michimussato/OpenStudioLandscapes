c38809e61e4ef31a654103513efd93b12ea5e0be
Michael Mussato <michimussato@gmail.com>
2025-11-21 11:56:12 +0100

    44-update-documentation: move pangolin specific code to its own module

---
+++ src/OpenStudioLandscapes/engine/utils/pangolin/__init__.py
@@ -0,0 +1,46 @@
+__all__ = [
+    "add_newt_service_to_compose_scope",
+]
+
+from typing import Dict
+
+from OpenStudioLandscapes.engine.utils.docker.compose_dicts import *
+from OpenStudioLandscapes.engine.enums import *
+
+
+def add_newt_service_to_compose_scope(
+    scrape_networks: Dict,
+    docker_dict_include: Dict,
+    compose_scope: ComposeScope,
+) -> None:
+    """
+    Updates `docker_dict_include` with Pangolin `newt` service(s)
+    and network(s) so that it directly connects itself with
+    a Pangolin Site.
+
+    Args:
+        compose_scope: ComposeScope
+        scrape_networks: dict of networks
+        docker_dict_include:
+
+    Returns: None
+    """
+    service_dict = get_pangolin_newt_service_skeleton(
+        compose_scope=compose_scope,
+    )
+
+    services = {
+        "services": {"newt": service_dict},
+    }
+
+    networks = {"networks": {"default": {"name": "pangolin_default"}}}
+
+    service_dict["networks"] = [
+        *networks["networks"].keys(),
+        *scrape_networks.keys(),
+    ]
+
+    docker_dict_include.update(services)
+    docker_dict_include.update(networks)
+
+    return None
