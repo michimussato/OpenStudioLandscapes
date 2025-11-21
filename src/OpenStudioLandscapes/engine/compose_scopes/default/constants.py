1be425a196bdf48da8d06c5cde7049b7e5fb1b10
Michael Mussato <michimussato@gmail.com>
2025-11-21 16:29:45 +0100

    44-update-documentation: adds compute_kind

--- src/OpenStudioLandscapes/engine/compose_scopes/default/constants.py
+++ src/OpenStudioLandscapes/engine/compose_scopes/default/constants.py
@@ -24,13 +24,14 @@ ATTACH_SITE_TO_COMPOSE_SCOPE = bool(
     int(os.environ.get("OPENSTUDIOLANDSCAPES__ATTACH_SITE_TO_COMPOSE_SCOPE", 0))
 )
 
+
 GROUP = f"{PREFIX_COMPOSE_SCOPE}_{str(COMPOSE_SCOPE)}"
 KEY = [GROUP]
 
 ASSET_HEADER = {
     "group_name": GROUP,
     "key_prefix": KEY,
-    # "compute_kind": "python",
+    "compute_kind": "python",
 }
 
 ENVIRONMENT = {}
