54b7e23e11ddb32442afdb0a27568930a7450e33
Michael Mussato <michimussato@gmail.com>
2025-12-12 11:12:42 +0100

    47-refactor-to-config-based-landscapes: wip

--- src/OpenStudioLandscapes/engine/discovery/get_feature_base_model.py
+++ src/OpenStudioLandscapes/engine/discovery/get_feature_base_model.py
@@ -1,4 +1,4 @@
-from typing import Dict, Type
+from typing import Dict, Type, List
 
 from dagster import (
     AssetExecutionContext,
@@ -10,33 +10,44 @@ from OpenStudioLandscapes.engine.discovery import discovery
 def get_feature_base_model(
     context: AssetExecutionContext,
     discovered_models: Dict[str, discovery.OpenStudioLandscapesDiscoveredFeature],
-    search_instance_type: Type[discovery.FeatureBaseModel,],
+    search_instance_type: Type[discovery.FeatureBaseModel],
 ) -> discovery.FeatureBaseModel:
     """
     We are not create a new Config object for this Feature. It
     was pre-made during the bootstrapping process.
     We just need to find it in the `discovery.DISCOVERED_MODELS` dict.
 
-    Find the OpenStudioLandscapesFeature from the discovered models
-    that matches the package name and return its Config object.
+    Find the `OpenStudioLandscapes.engine.config.models.FeatureBaseModel`
+    from the discovered models that matches the package name and
+    return its Config object.
 
     As all Feature Config objects are subclasses of
-    discovery.OpenStudioLandscapesFeature and also singletons,
-    there will always only be one Config object of type
+    `OpenStudioLandscapes.engine.config.models.FeatureBaseModel`
+    and also singletons, there will always only be one Config object of type
     <search_instance_type>.
 
     Returns:
-        Subclass of discovery.OpenStudioLandscapesFeature
+        Config Subclass of `OpenStudioLandscapes.engine.config.models.FeatureBaseModel`
 
     Raises:
-        ValueError
+        AssertionError
     """
 
-    for package, feature in discovered_models.items():
-        feature_config: discovery.FeatureBaseModel = feature.config
+    def filter_feature_config(
+        value: discovery.OpenStudioLandscapesDiscoveredFeature,
+    ):
+        feature_config: discovery.FeatureBaseModel = value.config
         if isinstance(feature_config, search_instance_type):
-            return feature_config
-    else:
-        msg = f"No Config object of type {type(search_instance_type)} found."
-        context.log.error(msg)
-        raise ValueError(msg)
+            return True
+        else:
+            return False
+
+    matches: List[discovery.FeatureBaseModel] = list(filter(filter_feature_config, discovered_models.values()))
+
+    assert len(matches) == 1, "Config object of type %s not identifiable: %i items found." % (type(search_instance_type), len(matches))
+
+    ret: Type[search_instance_type] = matches[0].config
+
+    assert isinstance(ret, search_instance_type), "%s is not a Config %s object." % (type(ret), type(search_instance_type))
+
+    return ret
