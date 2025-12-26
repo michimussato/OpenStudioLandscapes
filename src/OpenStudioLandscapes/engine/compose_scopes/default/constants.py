6dc798dd7ed04f39540c208815748fe8a86ae3c1
Michael Mussato <michimussato@gmail.com>
2025-12-26 13:48:36 +0100

    47-refactor-to-config-based-landscapes: update compose scopes

--- src/OpenStudioLandscapes/engine/compose_scopes/default/constants.py
+++ src/OpenStudioLandscapes/engine/compose_scopes/default/constants.py
@@ -1,11 +1,11 @@
-# import os
-#
-# ATTACH_PANGOLIN_SITE_TO_COMPOSE_SCOPE = bool(
-#     int(
-#         os.environ.get(
-#             "OPENSTUDIOLANDSCAPES__ATTACH_PANGOLINE_SITE_TO_COMPOSE_SCOPE", 0
-#         )
-#     )
-# )
+import os
+
+ATTACH_PANGOLIN_SITE_TO_COMPOSE_SCOPE = bool(
+    int(
+        os.environ.get(
+            "OPENSTUDIOLANDSCAPES__ATTACH_PANGOLINE_SITE_TO_COMPOSE_SCOPE", 0
+        )
+    )
+)
 
 COMPOSE_SCOPE_GROUP_PREFIX = "ComposeScope"
