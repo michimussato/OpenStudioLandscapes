499ebfb997273f8d7e25efe0cb8242529ade02a5
Michael Mussato <michimussato@gmail.com>
2025-12-28 16:44:46 +0100

    main: update community_channels.py

--- src/OpenStudioLandscapes/engine/community_channels.py
+++ src/OpenStudioLandscapes/engine/community_channels.py
@@ -13,311 +13,8 @@ community_channels = {
         },
         "discord": {
             "channel_name": "# openstudiolandscapes-general",
-            "channel_id": "1357343454065328202",
             "invite": "https://discord.gg/F6bDRWsHac",
         },
-        # "slack": {
-        #     "channel_name": "# openstudiolandscapes-general",
-        #     "channel_id": "C08LK80NBFF",
-        # },
-    },
-    "OpenStudioLandscapes-Ayon": {
-        "enabled": True,
-        "public": True,
-        "github": {
-            "repo_name": "OpenStudioLandscapes-Ayon",
-        },
-        "discord": {
-            "channel_name": "# openstudiolandscapes-ayon",
-            "channel_id": "1357722468336271411",
-            "invite": "https://discord.gg/gd6etWAF3v",
-        },
-        # "slack": {
-        #     "channel_name": "# openstudiolandscapes-ayon",
-        #     "channel_id": "C08LLBC7CB0",
-        # },
-    },
-    "OpenStudioLandscapes-Dagster": {
-        "enabled": True,
-        "public": True,
-        "github": {
-            "repo_name": "OpenStudioLandscapes-Dagster",
-        },
-        "discord": {
-            "channel_name": "# openstudiolandscapes-dagster",
-            "channel_id": "1358016764608249856",
-            "invite": "https://discord.gg/jwB3DwmKvs",
-        },
-        # "slack": {
-        #     "channel_id": "C08LZR5JFA6",
-        #     "channel_name": "# openstudiolandscapes-dagster",
-        # },
-    },
-    "OpenStudioLandscapes-Flamenco": {
-        "enabled": True,
-        "public": True,
-        "github": {
-            "repo_name": "OpenStudioLandscapes-Flamenco",
-        },
-        "discord": {
-            "channel_name": "# openstudiolandscapes-flamenco",
-            "channel_id": "1431746023293718549",
-            "invite": "https://discord.gg/EPrX5fzBCf",
-        },
-        # "slack": {
-        #     "channel_id": "C08LZR5JFA6",
-        #     "channel_name": "# openstudiolandscapes-dagster",
-        # },
-    },
-    "OpenStudioLandscapes-Flamenco-Worker": {
-        "enabled": True,
-        "public": True,
-        "github": {
-            "repo_name": "OpenStudioLandscapes-Flamenco-Worker",
-        },
-        "discord": {
-            "channel_name": "# openstudiolandscapes-flamenco-worker",
-            "channel_id": "1431746326634430484",
-            "invite": "https://discord.gg/Sa2zFqSc4p",
-        },
-        # "slack": {
-        #     "channel_id": "C08LZR5JFA6",
-        #     "channel_name": "# openstudiolandscapes-dagster",
-        # },
-    },
-    "OpenStudioLandscapes-Kitsu": {
-        "enabled": True,
-        "public": True,
-        "github": {
-            "repo_name": "OpenStudioLandscapes-Kitsu",
-        },
-        "discord": {
-            "channel_name": "# openstudiolandscapes-kitsu",
-            "channel_id": "1357638253632688231",
-            "invite": "https://discord.gg/6cc6mkReJ7",
-        },
-        # "slack": {
-        #     "channel_name": "# openstudiolandscapes-kitsu",
-        #     "channel_id": "C08L6M70ZB9",
-        # },
-    },
-    # "OpenStudioLandscapes-Deadline-10-2": {
-    #     "github": {
-    #         "repo_name": "OpenStudioLandscapes-Deadline-10-2",
-    #     },
-    #     "discord": {
-    #         "channel_id": "1358017409276973088",
-    #         "channel_name": "# openstudiolandscapes-deadline-10-2",
-    #         "invite": "https://discord.gg/p2UjxHk4Y3",
-    #     },
-    #     # "slack": {
-    #     #     "channel_id": "C08LZR963A6",
-    #     #     "channel_name": "# openstudiolandscapes-deadline-10-2",
-    #     # },
-    # },
-    # "OpenStudioLandscapes-Deadline-10-2-Worker": {
-    #     "github": {
-    #         "repo_name": "OpenStudioLandscapes-Deadline-10-2-Worker",
-    #     },
-    #     "discord": {
-    #         "channel_id": "1358024594409259059",
-    #         "channel_name": "# openstudiolandscapes-deadline-10-2-worker",
-    #         "invite": "https://discord.gg/ttkbfkzUmf",
-    #     },
-    #     # "slack": {
-    #     #     "channel_id": "C08LZSBM998",
-    #     #     "channel_name": "# openstudiolandscapes-deadline-10-2-worker",
-    #     # },
-    # },
-    # "OpenStudioLandscapes-filebrowser": {
-    #     "github": {
-    #         "repo_name": "OpenStudioLandscapes-filebrowser",
-    #     },
-    #     "discord": {
-    #         "channel_id": "1364746200175083520",
-    #         "channel_name": "# openstudiolandscapes-filebrowser",
-    #         "invite": "https://discord.gg/stzNsZBmwk",
-    #     },
-    #     # "slack": {
-    #     #     "channel_id": "",
-    #     #     "channel_name": "",
-    #     # },
-    # },
-    # "OpenStudioLandscapes-NukeRLM-8": {
-    #     "github": {
-    #         "repo_name": "OpenStudioLandscapes-NukeRLM-8",
-    #     },
-    #     "discord": {
-    #         "channel_id": "1358017656732782672",
-    #         "channel_name": "# openstudiolandscapes-nukerlm-8",
-    #         "invite": "https://discord.gg/bMVqrNrMg2",
-    #     },
-    #     # "slack": {
-    #     #     "channel_id": "C08LZDLFTMH",
-    #     #     "channel_name": "# openstudiolandscapes-nukerlm-8",
-    #     # },
-    # },
-    # "OpenStudioLandscapes-n8n": {
-    #     "github": {
-    #         "repo_name": "OpenStudioLandscapes-n8n",
-    #     },
-    #     "discord": {
-    #         "channel_id": "1420384480526602320",
-    #         "channel_name": "# openstudiolandscapes-n8n",
-    #         "channel_name": "https://discord.gg/zVFJUEaAwK",
-    #     },
-    #     # "slack": {
-    #     #     "channel_id": "",
-    #     #     "channel_name": "# openstudiolandscapes-n8n",
-    #     # },
-    # },
-    # "OpenStudioLandscapes-SESI-gcc-9-3-Houdini-20": {
-    #     "github": {
-    #         "repo_name": "OpenStudioLandscapes-SESI-gcc-9-3-Houdini-20",
-    #     },
-    #     "discord": {
-    #         "channel_id": "1358018027190484993",
-    #         "channel_name": "# openstudiolandscapes-sesi-gcc-9-3-houdini-20",
-    #         "invite": "https://discord.gg/Zwmn3VAMDx",
-    #     },
-    #     # "slack": {
-    #     #     "channel_id": "C08LUTR1WG5",
-    #     #     "channel_name": "# openstudiolandscapes-sesi-gcc-9-3-houdini-20",
-    #     # },
-    # },
-    "OpenStudioLandscapes-RustDeskServer": {
-        "enabled": True,
-        "public": True,
-        "github": {
-            "repo_name": "OpenStudioLandscapes-RustDeskServer",
-        },
-        "discord": {
-            "channel_id": "1414567463227621510",
-            "channel_name": "# openstudiolandscapes-rustdeskserver",
-            "invite": "https://discord.gg/nJ8Ffd2xY3",
-        },
-        # "slack": {
-        #     "channel_id": "C09E6HA0ZPW",
-        #     "channel_name": "# openstudiolandscapes-rustdeskserver",
-        # },
-    },
-    # "OpenStudioLandscapes-Teleport": {
-    #     "enabled": False,
-    #     "public": True,
-    #     "github": {
-    #         "repo_name": "OpenStudioLandscapes-Teleport",
-    #     },
-    #     "discord": {
-    #         "channel_id": "1420385295026884659",
-    #         "channel_name": "# openstudiolandscapes-teleport",
-    #         "invite": "https://discord.gg/SNMCw5aDfm",
-    #     },
-    #     # "slack": {
-    #     #     "channel_id": "C09E6HA0ZPW",
-    #     #     "channel_name": "# openstudiolandscapes-rustdeskserver",
-    #     # },
-    # },
-    # "OpenStudioLandscapes-Twingate": {
-    #     "enabled": True,
-    #     "public": False,
-    #     "github": {
-    #         "repo_name": "OpenStudioLandscapes-Twingate",
-    #     },
-    #     "discord": {
-    #         "channel_id": "1414768065174044844",
-    #         "channel_name": "# openstudiolandscapes-twingate",
-    #         "invite": "https://discord.gg/tREYa6UNJf",
-    #     },
-    #     # "slack": {
-    #     #     "channel_id": "C09E6HA0ZPW",
-    #     #     "channel_name": "# openstudiolandscapes-rustdeskserver",
-    #     # },
-    # },
-    # "OpenStudioLandscapes-Syncthing": {
-    #     "github": {
-    #         "repo_name": "OpenStudioLandscapes-Syncthing",
-    #     },
-    #     "discord": {
-    #         "channel_id": "1364746871381168138",
-    #         "channel_name": "# openstudiolandscapes-syncthing",
-    #         "invite": "https://discord.gg/upb9MCqb3X",
-    #     },
-    #     # "slack": {
-    #     #     "channel_id": "",
-    #     #     "channel_name": "",
-    #     # },
-    # },
-    # "OpenStudioLandscapes-Watchtower": {
-    #     "github": {
-    #         "repo_name": "OpenStudioLandscapes-Watchtower",
-    #     },
-    #     "discord": {
-    #         "channel_id": "1364747275938562079",
-    #         "channel_name": "# openstudiolandscapes-watchtower",
-    #         "invite": "https://discord.gg/5CQEjBaHg8",
-    #     },
-    #     # "slack": {
-    #     #     "channel_id": "",
-    #     #     "channel_name": "",
-    #     # },
-    # },
-    # "OpenStudioLandscapes-OpenCue": {
-    #     "github": {
-    #         "repo_name": "OpenStudioLandscapes-OpenCue",
-    #     },
-    #     "discord": {
-    #         "channel_id": "1379799680807997500",
-    #         "channel_name": "# openstudiolandscapes-opencue",
-    #         "invite": "https://discord.gg/3DdCZKkVyZ",
-    #     },
-    #     # "slack": {
-    #     #     "channel_id": "",
-    #     #     "channel_name": "",
-    #     # },
-    # },
-    # "OpenStudioLandscapes-OpenCue-Worker": {
-    #     "github": {
-    #         "repo_name": "OpenStudioLandscapes-OpenCue-Worker",
-    #     },
-    #     "discord": {
-    #         "channel_id": "",
-    #         "channel_name": "# openstudiolandscapes-opencue-worker",
-    #         "invite": "",
-    #     },
-    #     # "slack": {
-    #     #     "channel_id": "",
-    #     #     "channel_name": "",
-    #     # },
-    # },
-    # "OpenStudioLandscapes-Grafana": {
-    #     "github": {
-    #         "repo_name": "OpenStudioLandscapes-Grafana",
-    #     },
-    #     "discord": {
-    #         "channel_id": "1379800002179760159",
-    #         "channel_name": "# openstudiolandscapes-grafana",
-    #         "invite": "https://discord.gg/gEDQ8vJWDb",
-    #     },
-    #     # "slack": {
-    #     #     "channel_id": "",
-    #     #     "channel_name": "",
-    #     # },
-    # },
-    "OpenStudioLandscapes-VERT": {
-        "enabled": True,
-        "public": True,
-        "github": {
-            "repo_name": "OpenStudioLandscapes-VERT",
-        },
-        "discord": {
-            "channel_id": "1431597155973468321",
-            "channel_name": "# openstudiolandscapes-twingate",
-            "invite": "https://discord.gg/FYaFRUwbYr",
-        },
-        # "slack": {
-        #     "channel_id": "C09E6HA0ZPW",
-        #     "channel_name": "# openstudiolandscapes-rustdeskserver",
-        # },
     },
     # Template
     "OpenStudioLandscapes-Template": {
@@ -327,13 +24,206 @@ community_channels = {
             "repo_name": "OpenStudioLandscapes-Template",
         },
         "discord": {
-            "channel_id": "1414568696860512358",
             "channel_name": "# openstudiolandscapes-template",
             "invite": "https://discord.gg/J59GYp3Wpy",
         },
-        # "slack": {
-        #     "channel_id": "C09DMN2LH71",
-        #     "channel_name": "# openstudiolandscapes-rustdeskserver",
-        # },
+    },
+    "OpenStudioLandscapes-Ayon": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-Ayon",
+        },
+        "discord": {
+            "channel_name": "# openstudiolandscapes-ayon",
+            "invite": "https://discord.gg/gd6etWAF3v",
+        },
+    },
+    "OpenStudioLandscapes-Dagster": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-Dagster",
+        },
+        "discord": {
+            "channel_name": "# openstudiolandscapes-dagster",
+            "invite": "https://discord.gg/jwB3DwmKvs",
+        },
+    },
+    "OpenStudioLandscapes-Deadline-10-2": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-Deadline-10-2",
+        },
+        "discord": {
+            "channel_id": "1358017409276973088",
+            "invite": "https://discord.gg/p2UjxHk4Y3",
+        },
+    },
+    "OpenStudioLandscapes-Deadline-10-2-Worker": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-Deadline-10-2-Worker",
+        },
+        "discord": {
+            "channel_id": "1358024594409259059",
+            "invite": "https://discord.gg/ttkbfkzUmf",
+        },
+    },
+    "OpenStudioLandscapes-filebrowser": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-filebrowser",
+        },
+        "discord": {
+            "channel_id": "1364746200175083520",
+            "invite": "https://discord.gg/stzNsZBmwk",
+        },
+    },
+    "OpenStudioLandscapes-Flamenco": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-Flamenco",
+        },
+        "discord": {
+            "channel_name": "# openstudiolandscapes-flamenco",
+            "invite": "https://discord.gg/EPrX5fzBCf",
+        },
+    },
+    "OpenStudioLandscapes-Flamenco-Worker": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-Flamenco-Worker",
+        },
+        "discord": {
+            "channel_name": "# openstudiolandscapes-flamenco-worker",
+            "invite": "https://discord.gg/Sa2zFqSc4p",
+        },
+    },
+    "OpenStudioLandscapes-Grafana": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-Grafana",
+        },
+        "discord": {
+            "channel_id": "1379800002179760159",
+            "invite": "https://discord.gg/gEDQ8vJWDb",
+        },
+    },
+    "OpenStudioLandscapes-Kitsu": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-Kitsu",
+        },
+        "discord": {
+            "channel_name": "# openstudiolandscapes-kitsu",
+            "invite": "https://discord.gg/6cc6mkReJ7",
+        },
+    },
+    "OpenStudioLandscapes-LikeC4": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-LikeC4",
+        },
+        "discord": {
+            "channel_name": "# openstudiolandscapes-likec4",
+            "invite": "https://discord.gg/qAYYsKYF6V",
+        },
+    },
+    "OpenStudioLandscapes-NukeRLM-8": {
+        "enabled": False,
+        "public": False,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-NukeRLM-8",
+        },
+        "discord": {
+            "channel_name": "# openstudiolandscapes-nukerlm-8",
+            "invite": "https://discord.gg/bMVqrNrMg2",
+        },
+    },
+    "OpenStudioLandscapes-OpenCue": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-OpenCue",
+        },
+        "discord": {
+            "channel_name": "# openstudiolandscapes-opencue",
+            "invite": "https://discord.gg/3DdCZKkVyZ",
+        },
+    },
+    "OpenStudioLandscapes-OpenCue-Worker": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-OpenCue-Worker",
+        },
+        "discord": {
+            "channel_name": "# openstudiolandscapes-opencue-worker",
+            "invite": "https://discord.gg/n9fxxhHa3V",
+        },
+    },
+    "OpenStudioLandscapes-RustDeskServer": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-RustDeskServer",
+        },
+        "discord": {
+            "channel_name": "# openstudiolandscapes-rustdeskserver",
+            "invite": "https://discord.gg/nJ8Ffd2xY3",
+        },
+    },
+    "OpenStudioLandscapes-SESI-gcc-9-3-Houdini-20": {
+        "enabled": False,
+        "public": False,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-SESI-gcc-9-3-Houdini-20",
+        },
+        "discord": {
+            "channel_name": "# openstudiolandscapes-sesi-gcc-9-3-houdini-20",
+            "invite": "https://discord.gg/Zwmn3VAMDx",
+        },
+    },
+    "OpenStudioLandscapes-Syncthing": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-Syncthing",
+        },
+        "discord": {
+            "channel_name": "# openstudiolandscapes-syncthing",
+            "invite": "https://discord.gg/upb9MCqb3X",
+        },
+    },
+    "OpenStudioLandscapes-Watchtower": {
+        "enabled": False,
+        "public": False,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-Watchtower",
+        },
+        "discord": {
+            "channel_name": "# openstudiolandscapes-watchtower",
+            "invite": "https://discord.gg/5CQEjBaHg8",
+        },
+    },
+    "OpenStudioLandscapes-VERT": {
+        "enabled": True,
+        "public": True,
+        "github": {
+            "repo_name": "OpenStudioLandscapes-VERT",
+        },
+        "discord": {
+            "channel_name": "# openstudiolandscapes-vert",
+            "invite": "https://discord.gg/EPrX5fzBCf",
+        },
     },
 }
