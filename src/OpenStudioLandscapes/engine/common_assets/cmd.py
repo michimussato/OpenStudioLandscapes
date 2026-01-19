bec587e4e958a0f360378942ee1d531c3e6b8f44
Michael Mussato <michimussato@gmail.com>
2026-01-19 13:50:12 +0100

    - adds feature cmd factory

---
+++ src/OpenStudioLandscapes/engine/common_assets/cmd.py
@@ -0,0 +1,33 @@
+from typing import Dict, List
+
+from dagster import (
+    AssetsDefinition,
+    OpDefinition,
+    Out,
+)
+
+from OpenStudioLandscapes.engine.base.ops.factories import factory_cmd
+
+
+def get_feature__cmd(
+    ASSET_HEADER: Dict,
+) -> AssetsDefinition:
+
+    cmd_op: OpDefinition = factory_cmd(
+        name=f"op_feature__cmd__{ASSET_HEADER['group_name']}",
+        ins={},
+        out={
+            "cmd_append": Out(Dict),
+            "cmd_extend": Out(List),
+        },
+    )
+
+    cmd: AssetsDefinition = AssetsDefinition.from_op(
+        cmd_op,
+        group_name=ASSET_HEADER["group_name"],
+        key_prefix=ASSET_HEADER["key_prefix"],
+        keys_by_input_name={},
+        keys_by_output_name={},
+    )
+
+    return cmd
