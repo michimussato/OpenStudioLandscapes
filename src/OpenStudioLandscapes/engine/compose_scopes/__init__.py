6d17bcc2b4a50f1010ef4513126310a3ce842a7b
Michael Mussato <michimussato@gmail.com>
2026-02-12 12:49:49 +0100

    - Better check for alloy wrapper whether Grafana Feature is enabled or not

--- src/OpenStudioLandscapes/engine/compose_scopes/__init__.py
+++ src/OpenStudioLandscapes/engine/compose_scopes/__init__.py
@@ -1,5 +1,25 @@
+from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
 from OpenStudioLandscapes.engine.discovery import discovery
+from dagster import get_dagster_logger
 
-GRAFANA_AVAILABLE: bool = (
-    "OpenStudioLandscapes.Grafana" in discovery.DISCOVERED_MODELS.keys()
+LOGGER = get_dagster_logger(__name__)
+
+grafana_installed: bool = "OpenStudioLandscapes.Grafana" in discovery.DISCOVERED_MODELS.keys()
+LOGGER.info(f"{grafana_installed = }")
+
+if grafana_installed:
+    grafana_config: FeatureBaseModel = discovery.DISCOVERED_MODELS["OpenStudioLandscapes.Grafana"].config
+    LOGGER.info(f"{grafana_config = }")
+
+    grafana_enabled: bool = grafana_config.enabled
+    LOGGER.info(f"{grafana_enabled = }")
+else:
+    grafana_enabled: bool = False
+    LOGGER.info(f"{grafana_enabled = }")
+
+GRAFANA_AVAILABLE: bool = all(
+    [
+        grafana_installed,
+        grafana_enabled,
+    ],
 )
