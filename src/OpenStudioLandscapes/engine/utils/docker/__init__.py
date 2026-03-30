972d798935297b88b2691c12bfd807c118efe157
Michael Mussato <michimussato@gmail.com>
2026-03-30 12:23:44 +0200

    - break out streaming_process

--- src/OpenStudioLandscapes/engine/utils/docker/__init__.py
+++ src/OpenStudioLandscapes/engine/utils/docker/__init__.py
@@ -5,14 +5,12 @@ __all__ = [
 ]
 
 import pathlib
-import queue
-import shlex
 import shutil
-import subprocess
-import threading
-from typing import Any, Generator, List, Union
+from typing import List, Union
 
 from dagster import AssetExecutionContext, OpExecutionContext, get_dagster_logger
+
+from OpenStudioLandscapes.Dagster.streaming_process import submit_cmds
 
 LOGGER = get_dagster_logger(__name__)
 
@@ -93,93 +91,8 @@ def docker_push_cmd(
     return push_cmds
 
 
-class OutputReader(threading.Thread):
-    def __init__(self, stream, output_queue):
-        threading.Thread.__init__(self)
-        self.stream = stream
-        self.output_queue = output_queue
-
-    def run(self):
-        for line in iter(self.stream.readline, b""):
-            self.output_queue.put(line.decode().strip())
-
-
-def execute_in_threads(
-    command: str,
-) -> Generator[str | Any, None, None]:
-
-    process = subprocess.Popen(
-        command,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-
-    stdout_queue = queue.Queue()
-    stderr_queue = queue.Queue()
-
-    stdout_reader = OutputReader(process.stdout, stdout_queue)
-    stderr_reader = OutputReader(process.stderr, stderr_queue)
-
-    stdout_reader.start()
-    stderr_reader.start()
-
-    while True:
-        # Todo:
-        #  - [ ] could run in two threads
-        while not stdout_queue.empty():
-            stdout = "stdout: %s" % stdout_queue.get()
-            yield stdout
-        while not stderr_queue.empty():
-            stderr = "stderr: %s" % stderr_queue.get()
-            yield stderr
-
-        returncode = process.poll()
-        if returncode is not None:
-            returncode_msg = "return code: %i" % returncode
-            if returncode != 0:
-                raise OpenStudioLandscapesDockerException(
-                    f"Image not built successfully. {returncode = }"
-                )
-            yield returncode_msg
-            break
-
-
-def docker_process_cmds(
-    context: Union[OpExecutionContext, AssetExecutionContext],
-    cmds: List[List[str]],
-) -> Generator[str | Any, Any, None]:
-
-    for cmd in cmds:
-
-        context.log.info(f"Processing command: \"{' '.join(cmd)}\"")
-
-        for s in execute_in_threads(
-            command=shlex.join(cmd),
-        ):
-            yield s
-
-
-def docker_do(
-    context: Union[OpExecutionContext, AssetExecutionContext],
-    cmds: List[List[str]],
-) -> List[str]:
-    """
-    Args:
-        context: Union[OpExecutionContext, AssetExecutionContext]
-        cmds: list of commands to execute
-
-    Returns:
-        list[str]: all collected records (stdout, stderr, return code)
-    """
-
-    records = []
-
-    for record in docker_process_cmds(
-        context=context,
-        cmds=cmds,
-    ):
-        context.log.info(record)
-        records.append(record)
-
-    return records
+# An alias for submit_cmds so that we don't
+# have to refactor all OpenStudioLandscapes (yet)
+# Todo
+#  - [ ] replace references to `docker_do`
+docker_do = submit_cmds
