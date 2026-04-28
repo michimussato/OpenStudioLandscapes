92a5b8355c5d5c67348836361aeb476665375b78
Michael Mussato <michimussato@gmail.com>
2026-04-28 15:50:25 +0200

    remove non-effective default for openstudiolandscapes__rez_config field

--- src/OpenStudioLandscapes/engine/config/models.py
+++ src/OpenStudioLandscapes/engine/config/models.py
@@ -478,11 +478,17 @@ class ConfigEngine(BaseConfig):
         default=RezConfigModel(
             **{
                 "deploy_rez": True,
-                "REZ_PACKAGES_PATH": [
-                    pathlib.Path("~/rez/packages/local"),
-                    pathlib.Path("~/rez/packages/deployed/internal"),
-                    pathlib.Path("/data/share/rez-packages/packages"),
-                ],
+                # The following has no effect here:
+                # REZ_PACKAGES_PATH refers to a property, not a Field
+                # Hence, model gets created using defaults for:
+                # - REZ_LOCAL_PACKAGES_PATH
+                # - REZ_RELEASE_PACKAGES_PATH
+                # - REZ_EXTERNAL_PACKAGES_PATH
+                # "REZ_PACKAGES_PATH": [
+                #     pathlib.Path("~/rez/packages/local"),
+                #     pathlib.Path("~/rez/packages/deployed/internal"),
+                #     pathlib.Path("/data/share/rez-packages/packages"),
+                # ],
             },
         ),
     )
