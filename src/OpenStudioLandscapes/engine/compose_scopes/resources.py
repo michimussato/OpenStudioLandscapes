3d927135d9ca6db16c9b8dbb615d8e1883a9afd4
Michael Mussato <michimussato@gmail.com>
2026-08-10 09:54:11 +0200

    test successful

---
+++ src/OpenStudioLandscapes/engine/compose_scopes/resources.py
@@ -0,0 +1,8 @@
+from OpenStudioLandscapes.engine.compose_scopes.configurable_resources.config_compose_scope import (
+    config_ConfigComposeScopeConfigurableResource,
+)
+
+
+configurable_resources_base = {
+    "config_ConfigComposeScopeConfigurableResource": config_ConfigComposeScopeConfigurableResource,
+}
